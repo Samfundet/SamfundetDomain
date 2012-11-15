392cc2409c43ca4019afc2ad3acf3270d0514006
Jonas Amundsen <jonasba@gmail.com>
2012-11-15 20:56:21 +0100

    Added configurable database option.

--- lib/samfundet_domain.rb
+++ lib/samfundet_domain.rb
@@ -1,4 +1,24 @@
+require "active_support/configurable"
 require "samfundet_domain/engine"
 
 module SamfundetDomain
+  include ActiveSupport::Configurable
+
+  class << self
+    def setup
+      yield config
+
+      database_path = "#{Rails.root}/config/database.yml"
+
+      if File.exists? database_path
+        database_config = YAML.load_file database_path
+
+        if config.domain_database && false
+          [Area, Group, GroupType].each do |model|
+            model.establish_connection database_config[config.domain_database.to_s]
+          end
+        end
+      end
+    end
+  end
 end
