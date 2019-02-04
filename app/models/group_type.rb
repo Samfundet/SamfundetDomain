13f42c73fb822c3f62ec0c811627c1f09ede23cb
alfiehub <alfiehub@users.noreply.github.com>
2019-02-04 14:36:53 +0100

    attr_accessible is deprecated in favor of strong params

--- app/models/group_type.rb
+++ app/models/group_type.rb
@@ -1,8 +1,6 @@
 class GroupType < ActiveRecord::Base
   has_many :groups, :order => 'name'
 
-  #attr_accessible :description, :priority
-
   validates_presence_of :description, :priority
 
   default_scope { order(priority: :desc) }
